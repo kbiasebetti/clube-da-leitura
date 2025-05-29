bc04c0e136b29e7446068e5776b92f9af399e6c5
Kauã Biasebetti <kauamengel17@gmail.com>
2025-05-29 11:13:23 -0300

    Implementada classe RepositorioBase com operações CRUD para entidades

---
+++ Compartilhado/RepositorioBase.cs
@@ -0,0 +1,86 @@
+namespace ClubeDaLeitura.Compartilhado
+
+public abstract class RepositorioBase
+{
+    private EntidadeBase[] registros = new EntidadeBase[100];
+    private int proximoId;
+
+    public virtual void Inserir(EntidadeBase entidade)
+    {
+        int indiceParaInserir = -1;
+
+        entidade.id = proximoId;
+
+        for (int i = 0; i < registros.Length, i++)
+        {
+            if (registros[i] == null)
+            {
+                indiceParaInserir = i;
+                break;
+            }
+        }
+
+        if (indiceParaInserir != indiceParaInserir)
+        {
+            registros[indiceParaInserir] = entidade;
+            proximoId++;
+        }
+        else
+        {
+            Console.WriteLine("ERRO: Repositório cheio, não foi possível inserir!");
+        }
+    }
+
+    public virtual void Editar(int idSelecionado, EntidadeBase entidadeAtualizada)
+    {
+        for (int i = 0; i < registros.Length; i++)
+        {
+            if (registros[i] != null && registros.id == idSelecionado)
+            {
+                entidadeAtualizada.id = idSelecionado;
+                registros[i] = entidadeAtualizada;
+                return;
+            }
+        }
+    }
+
+    public virtual bool Excluir(int idSelecionado)
+    {
+        for (int i = 0; registros.Lenght; i++)
+        {
+            if (registros != null && registros.id = idSelecionado)
+            {
+                registros[i] = null;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public virtual EntidadeBase SelecionarPorId(int idSelecionado)
+    {
+        foreach (EntidadeBase entidade in registros)
+        {
+            if (entidade != null && entidade.id == idSelecionado)
+            {
+                return entidade
+            }
+        }
+
+        return null;
+    }
+
+    public virtual EntidadeBase SelecionarTodos()
+    {
+        foreach (EntidadeBase entidade in registros)
+        {
+            if (entidade != null)
+            {
+                return registros;
+            }
+        }
+
+        return null;
+    }
+}
